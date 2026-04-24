5a6de21b3f99cddd065e97eedc04ed1adca30978
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2026-04-24 14:15:15 +0000

    :sparkles: add publisher config

--- cmd/publisher.go
+++ cmd/publisher.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+	"pubsub-sample/config"
 	"pubsub-sample/util"
 
 	"github.com/spf13/cobra"
@@ -10,7 +12,10 @@ func newPublisherCmd() *cobra.Command {
 	publisherCmd := &cobra.Command{
 		Use: "publisher",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			util.InfoLog("publisher Called!")
+			publisherConfig := config.NewPublisherConfig()
+			util.InfoLog(
+				fmt.Sprintf("publisher called: %v", publisherConfig),
+			)
 			return nil
 		},
 	}
